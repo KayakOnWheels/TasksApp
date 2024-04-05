06dca106d1d7128d08a4a91c05a10faf36194175
KayakOnWheels <115392859+KayakOnWheels@users.noreply.github.com>
2024-04-05 14:48:18 +0200

    Added global error handling and implemented deleteTask and updateTask

--- tasks/src/main/java/com/crud/tasks/service/DbService.java
+++ tasks/src/main/java/com/crud/tasks/service/DbService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.exceptions.TaskNotFoundException;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTaskById(final long id) {
-        return repository.findById(id);
+    public Task getTask(final long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
     }
 
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final long taskId) {
+        repository.deleteById(taskId);
+    }
 }
